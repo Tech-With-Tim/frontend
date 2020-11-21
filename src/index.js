f680713bc77505e3a15b85c25444d723b1adb9b7
mohamed040406 <55202038+mohamed040406@users.noreply.github.com>
2020-11-21 22:51:02 +0200

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1>Hello World</h1>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
