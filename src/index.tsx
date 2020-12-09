a54220968f55a4991ed147acd316e9d63ebb1554
mohamed040406 <55202038+mohamed040406@users.noreply.github.com>
2020-12-09 19:48:57 +0200

    added typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Hello World</h1>
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
