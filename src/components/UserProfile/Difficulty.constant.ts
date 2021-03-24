bbff69da5f548467e943395c4ce003e83b099b0a
AadityaMunjal <aadityamunjal06@gmail.com>
2021-03-24 21:35:57 +0530

    common constant file for UserProfile

--- src/components/UserProfile/Difficulty.constant.ts
+++
@@ -1,5 +0,0 @@
-export const DIFFICULTY = {
-  easy: "easy",
-  medium: "medium",
-  hard: "hard",
-};
---
+++ src/components/UserProfile/constants.ts
@@ -0,0 +1,11 @@
+export const DIFFICULTY = {
+  easy: "easy",
+  medium: "medium",
+  hard: "hard",
+};
+
+export const BADGENAMES = {
+  Badge1: "Badge1",
+  Badge2: "Badge2",
+  Badge3: "Badge3",
+};
