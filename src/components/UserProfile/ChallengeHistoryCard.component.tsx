41a04f45dae09209101b26a926d7edfaad4a76c2
AadityaMunjal <aadityamunjal06@gmail.com>
2021-03-25 12:24:28 +0530

    update challenge-history-card

--- src/components/UserProfile/ChallengeHistoryCard.component.tsx
+++ src/components/UserProfile/ChallengeHistoryCard.component.tsx
@@ -1,19 +1,25 @@
 import React from "react";
 import { ChallengeHistoryCardStyle } from "./Styles";
-import { DIFFICULTY } from "./Difficulty.constant";
+import { DIFFICULTY } from "./constants";
 
-function ChallengeHistoryCard(props) {
+interface Props {
+  difficulty: string;
+  solution: string;
+  no: number;
+}
+
+function ChallengeHistoryCard({ difficulty, solution, no }: Props) {
   const difficultyClassNames = [
     "dot" + " green",
-    "dot" + (props.difficulty !== DIFFICULTY.easy ? " yellow" : ""),
-    "dot" + (props.difficulty === DIFFICULTY.hard ? " red" : ""),
-    props.difficulty,
+    "dot" + (difficulty !== DIFFICULTY.easy ? " yellow" : ""),
+    "dot" + (difficulty === DIFFICULTY.hard ? " red" : ""),
+    difficulty,
   ];
   console.log(difficultyClassNames);
   return (
     <ChallengeHistoryCardStyle>
-      <div className={props.solution + " solution-card"}>
-        <h5>{"#" + props.no + " " + props.solution}</h5>
+      <div className={solution + " solution-card"}>
+        <h5>{"#" + no + " " + solution}</h5>
       </div>
       <div className={"difficulty-meter"}>
         <div className={difficultyClassNames[0]}></div>
