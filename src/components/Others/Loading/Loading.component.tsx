45a92e1bb339362818cdbc7aade55081f5a384c5
AadityaMunjal <aadityamunjal06@gmail.com>
2021-03-26 20:27:05 +0530

    custom loading

--- src/components/Others/Loading/Loading.component.tsx
+++ src/components/Others/Loading/Loading.component.tsx
@@ -1,25 +1,14 @@
 import React from "react";
-import Backdrop from "@material-ui/core/Backdrop";
-import CircularProgress from "@material-ui/core/CircularProgress";
-import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import { Overlay, Spinner } from "./Styles";
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    backdrop: {
-      zIndex: theme.zIndex.drawer + 1,
-      color: "#fff",
-    },
-  })
-);
-
-export default function SimpleBackdrop() {
-  const classes = useStyles();
-
+function Loading() {
   return (
-    <div>
-      <Backdrop className={classes.backdrop} open>
-        <CircularProgress color="inherit" />
-      </Backdrop>
-    </div>
+    <>
+      <Overlay>
+        <Spinner />
+      </Overlay>
+    </>
   );
 }
+
+export default Loading;
