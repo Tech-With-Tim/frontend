1ecaffebcaded733627df9316b92e1e3f09c9eff
mohamed040406 <55202038+mohamed040406@users.noreply.github.com>
2021-04-13 14:59:18 +0200

    add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
