016a99ebb0d6f4713ae8ac73f4dcb8b89876ebbd
mohamed040406 <55202038+mohamed040406@users.noreply.github.com>
2021-04-15 13:24:21 +0200

    Add Storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,16 @@
+import { themes } from "@storybook/theming";
+
+import "../src/index.scss";
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  docs: {
+    theme: themes.dark,
+  },
+};
