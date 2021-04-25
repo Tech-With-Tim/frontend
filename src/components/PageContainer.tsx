d6227a76586448e4a456501421bbfa30b9803f5c
AadityaMunjal <aadityamunjal06@gmail.com>
2021-04-25 23:21:49 +0530

    fix lint

--- src/components/PageContainer.tsx
+++ src/components/PageContainer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const PageContainer = ({ children }) => {
+const PageContainer: React.FC = ({ children }) => {
   return <div className="container my-5 flex flex-col gap-24 mdlg:max-w-4xl">{children}</div>;
 };
 
