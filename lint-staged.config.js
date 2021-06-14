8d7e856ed94e919ddb81c00c9f8ba94630c0eb1d
mohamed040406 <55202038+mohamed040406@users.noreply.github.com>
2021-06-14 18:28:58 +0200

    :green_heart: update pre-commit hooks

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   // Run type-check on changes to TypeScript files
-  "**/*.ts?(x)": () => "npm run type-check",
+  "**/*.ts?(x)": () => "yarn run type-check",
   // Run ESLint on changes to JavaScript/TypeScript files
-  "**/*.(ts|js)?(x)": (filenames) => `npm run lint:fix ${filenames.join(" ")}`,
-  "**/*": "npm run test",
+  "**/*.(ts|js)?(x)": () => "yarn run lint",
 };
