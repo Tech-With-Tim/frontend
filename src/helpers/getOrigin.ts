ffe0efe29c6f043f513d46844242c19a0f027b8a
mohamed040406 <55202038+mohamed040406@users.noreply.github.com>
2021-06-15 10:42:56 +0200

    Whoops

--- src/helpers/getOrigin.ts
+++ src/helpers/getOrigin.ts
@@ -1,9 +1,5 @@
 export const getOrigin = (): string => {
   if (process.env.NODE_ENV == "development") return "http://localhost:3000";
 
-  if (!process.env.NEXT_PUBLIC_ORIGIN) {
-    throw new Error("No env variable named ORIGIN found. Please add this to your env.");
-  }
-
   return process.env.NEXT_PUBLIC_ORIGIN;
 };
