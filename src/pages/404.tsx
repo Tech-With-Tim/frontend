67757ae209ee9a481136c3449eb3791d58691d38
mohamed040406 <mohamed040406@users.noreply.github.com>
2021-09-07 18:05:14 +0000

    temp 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = (): JSX.Element => {
+  return (
+    <div className="mx-auto w-max mt-32">
+      <h1 className="text-7xl text-primary font-bold text-center">404</h1>
+      <h3 className="text-gray-400">The Page you are looking for couldn&apos;t be found :(</h3>
+    </div>
+  );
+};
+
+export default NotFound;
